b8c8d3bf1bed58e54f211e58f5f0959120d7c43d
Bugen Zhao <i@bugenzhao.com>
2021-03-28 07:35:20 +0000

    add back some files

--- frontend-react/src/services/index.tsx
+++ frontend-react/src/services/index.tsx
@@ -3,16 +3,16 @@ import _ from 'lodash';
 import _BOOKS from '../resources/books.json';
 
 type StoreContextType = {
-    cart: number[],
-    addToCart: (id: number) => void,
-    setCart: (newCart: number[]) => void,
+  cart: number[],
+  addToCart: (id: number) => void,
+  setCart: (newCart: number[]) => void,
 }
 export const StoreContext = createContext<StoreContextType>(null!);
 
 export const ALL_BOOKS = _(_BOOKS).keyBy("id").value();
 type BooksContextType = {
-    BOOKS: typeof ALL_BOOKS,
-    updateBOOKS: (c: (d: typeof ALL_BOOKS) => void) => void,
+  BOOKS: typeof ALL_BOOKS,
+  updateBOOKS: (c: (d: typeof ALL_BOOKS) => void) => void,
 }
 export const BooksContext = createContext<BooksContextType>(null!);
 
@@ -20,6 +20,6 @@ export type Book = typeof _BOOKS[0];
 
 
 export type SelectContextType<T> = {
-    selected: T,
-    setSelected: (n: T) => void,
+  selected: T,
+  setSelected: (n: T) => void,
 };
