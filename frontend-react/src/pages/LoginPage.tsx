7d02e89c38011da1fb2755b5c07034f7dcd65bcf
Bugen Zhao <i@bugenzhao.com>
2021-03-28 14:29:36 +0000

    react: add sign in tooltip

--- frontend-react/src/pages/LoginPage.tsx
+++ frontend-react/src/pages/LoginPage.tsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import { useHistory } from "react-router-dom";
 import { useStore } from "../services";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 export function LoginPage() {
   const { user, setUser, setCart } = useStore();
@@ -20,14 +21,24 @@ export function LoginPage() {
         }}
       >
         <h1 className="h1 mb-3 fw-bold">Sign in</h1>
-        <input
-          className="form-control"
-          placeholder="Username"
-          required={true}
-          autoFocus={true}
-          defaultValue={user === "" ? "Guest" : user}
-          ref={userInputRef}
-        />
+        <OverlayTrigger
+          placement="left"
+          overlay={
+            <Tooltip id="user-tooltip">
+              Sign in as <strong>Bugen</strong> to get the admin permission.
+            </Tooltip>
+          }
+        >
+          <input
+            className="form-control"
+            placeholder="Username"
+            required={true}
+            autoFocus={true}
+            defaultValue={user === "" ? "Guest" : user}
+            ref={userInputRef}
+          />
+        </OverlayTrigger>
+
         <input
           type="password"
           className="form-control"
